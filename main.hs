5a8712362d348be59fdf32d9b45b6cc7b160c547
Sergio Vago <sergio.vago@hotmail.com>
2022-03-24 10:18:18 -0300

    Ajuste na leitura de arquivos

--- main.hs
+++ main.hs
@@ -4,7 +4,7 @@ import ProcessaArquivos (processaArquivosERetornaOsResultados)
 
 main :: IO ()
 main = do
-  let nomeCord = "bateria2/nome-coord.txt"
-  let diariaCusto = "bateria2/diaria-custo.txt"
+  let nomeCord = "nome-coord.txt"
+  let diariaCusto = "diaria-custo.txt"
 
   processaArquivosERetornaOsResultados nomeCord diariaCusto
